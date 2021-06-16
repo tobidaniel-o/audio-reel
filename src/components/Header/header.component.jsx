3cfbdacce0373768cb14dc5324913a888caf1108
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-06-16 09:43:39 -0400

    created project with create react app and added files for homepage

---
+++ src/components/Header/header.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./header.styles.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="new-product">
+        <h1>new product</h1>
+        <span>xx99 mark ii</span>
+        <span>headphones</span>
+        <p>
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/Header/header.component";
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default HomePage;
