aec7d85fd7fadb25a985d5d6df93620b596cb976
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-06-17 10:10:09 -0400

    added shared section to the main section

--- src/components/Sections/Section-Main/Section-Shared/section-shared.component.jsx
+++ src/components/Sections/Section-Main/Section-Shared/section-shared.component.jsx
@@ -4,14 +4,14 @@ import "./section-shared.styles.scss";
 function SectionShared() {
   return (
     <section className="section-shared">
-      <div>
-        <div>
-          <p>bringing you the</p>
-          <p>
-            <span>best</span>audio gear
+      <article className="article-main">
+        <div className="article-container">
+          <p className="article-heading-one">bringing you the</p>
+          <p className="article-heading-one">
+            <span>best</span> audio gear
           </p>
         </div>
-        <p>
+        <p className="article">
           Located at the heart of New York City, Audioreel is the premier store
           for high end headphones, earphones, speakers, and audio accessories.
           We have a large showroom and luxury demonstration rooms available for
@@ -19,10 +19,13 @@ function SectionShared() {
           store to meet some of the fantastic people who make Audioreel the best
           place to buy your portable audio equipment.
         </p>
-      </div>
-      <div>
+      </article>
+      <div className="article-image">
         <img
-          src=""
+          src={
+            require("../../../../assets/shared/desktop/image-best-gear.jpg")
+              .default
+          }
           alt="A guy with a headphone across his head listening to music."
         />
       </div>
