ae5ddddd7bd21c5bde9a09b7cf598906afc13af3
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-06-20 15:07:19 -0400

    created and added the section-two component

--- src/ProductDetail/Headphones-1/headphones-1.component.jsx
+++ src/ProductDetail/Headphones-1/headphones-1.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from "../../components/Navigation/navigation.component";
 import SectionOne from "../components/SectionOne/section-one-component";
+import SectionTwo from "../components/SectionTwo/section-two-component";
 
 import "./headphones-1.styles.scss";
 
@@ -26,7 +27,31 @@ function HeadPhones1() {
           price="$2,999"
           numberOfItem="1"
         />
-        
+        <SectionTwo
+          firstFeature="Featuring a genuine leather head strap and premium earcups, these
+            headphones deliver superior comfort for those who like to enjoy
+            endless listening. It includes intuitive controls designed for any
+            situation. Whether you’re taking a business call or just in your own
+            personal space, the auto on/off and pause features ensure that
+            you’ll never miss a beat."
+          secondFeature="The advanced Active Noise Cancellation with built-in equalizer allow
+            you to experience your audio world on your terms. It lets you enjoy
+            your audio in peace, but quickly interact with your surroundings
+            when you need to. Combined with Bluetooth 5. 0 compliant
+            connectivity and 17 hour battery life, the XX99 Mark II headphones
+            gives you superior sound, cutting-edge technology, and a modern
+            design aesthetic. "
+          firstNumberOfUnits="1x"
+          firstUnitType="Headphone Unit"
+          secondNumberOfUnits="2x"
+          secondUnitType="Replacement Earcups"
+          thirdNumberOfUnits="1x"
+          thirdUnitType="User Manual"
+          fourthNumberOfUnits="1x"
+          fourthUnitType="3.5mm 5m Audio Cable"
+          fifthNumberOfUnits="1x"
+          fifthUnitType="Travel Bag"
+        />
       </div>
     </>
   );
--- src/ProductDetail/components/SectionTwo/section-two-component.jsx
+++ src/ProductDetail/components/SectionTwo/section-two-component.jsx
@@ -1,12 +1,57 @@
-import React from 'react'
-import './section-two.styles.scss'
+import React from "react";
+import "./section-two.styles.scss";
 
-function SectionTwo() {
+function SectionTwo({
+  firstFeature,
+  secondFeature,
+  firstNumberOfUnits,
+  firstUnitType,
+  secondNumberOfUnits,
+  secondUnitType,
+  thirdNumberOfUnits,
+  thirdUnitType,
+  fourthNumberOfUnits,
+  fourthUnitType,
+  fifthNumberOfUnits,
+  fifthUnitType,
+}) {
   return (
     <>
-      
+      <main className="main">
+        <article className="article">
+          <h2>features</h2>
+          <p>{firstFeature}</p>
+          <p>{secondFeature}</p>
+        </article>
+
+        <aside className="aside">
+          <h2>in the box</h2>
+          <ul>
+            <li>
+              <span>{firstNumberOfUnits}</span>
+              {firstUnitType}
+            </li>
+            <li>
+              <span>{secondNumberOfUnits}</span>
+              {secondUnitType}
+            </li>
+            <li>
+              <span>{thirdNumberOfUnits}</span>
+              {thirdUnitType}
+            </li>
+            <li>
+              <span>{fourthNumberOfUnits}</span>
+              {fourthUnitType}
+            </li>
+            <li>
+              <span>{fifthNumberOfUnits}</span>
+              {fifthUnitType}
+            </li>
+          </ul>
+        </aside>
+      </main>
     </>
-  )
+  );
 }
 
-export default SectionTwo
+export default SectionTwo;
