6830d9495eb41b57792f7dc7ef9901bf33b2f55e
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-07-02 22:42:44 -0400

    updated

--- src/components/Sections/sections.component.jsx
+++ src/components/Sections/sections.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import SectionMiniDetails from './Section-Mini-Details/section-mini-details.component'
-// import SectionMain from './Section-Main/section-main.component'
-import SectionMain from '../Sections/Section-Main/section-main.component'
+import SectionMain from './Section-Main/section-main.component'
 
 function Sections() {
   return (
     <div className="wrapper">
       <SectionMiniDetails />
       <SectionMain />
+      
     </div>
   )
 }
