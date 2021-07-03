4d1bb3aa1ed8c8537295b29ba0cbedfb5f89056e
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-07-02 22:09:12 -0400

    updated the file

--- src/components/Sections/sections.component.jsx
+++ src/components/Sections/sections.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import SectionMiniDetails from './Section-Mini-Details/section-mini-details.component'
 // import SectionMain from './Section-Main/section-main.component'
-import SectionMain from './Section-Main/section-main.component'
+import SectionMain from '../Sections/Section-Main/section-main.component'
 
 function Sections() {
   return (
