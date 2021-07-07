244225bd9f275ad0498d7df6c09fb454438ff181
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-07-06 22:30:41 -0400

    added the redux library

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./ScrollToTop";
+import { Provider } from "react-redux";
+
 
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ScrollToTop />
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
