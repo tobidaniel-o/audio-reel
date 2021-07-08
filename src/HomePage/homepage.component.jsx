a6a11d7609d90e030d936cccbd61c20be8b392be
Tobi Okunuga <tobi.daniel2018@gmail.com>
2021-07-08 00:51:07 -0400

    update

--- src/HomePage/homepage.component.jsx
+++ src/HomePage/homepage.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "../components/Header/header.component";
 import Sections from "../components/Sections/sections.component";
 import Footer from "../components/Footer/footer.component";
-import Navigation from "../components/Navigation/navigation.component";
 
 function HomePage() {
   return (
--- src/components/Footer/footer.component.jsx
+++ src/components/Footer/footer.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Navigation from "../Navigation/navigation.component";
 
 import "./footer.styles.scss";
